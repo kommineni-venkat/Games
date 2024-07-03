f4c5cab91ad61f691b32cae343af0a9cec1f1586
kommineni-venkat <komminenivenkat101@gmail.com>
2024-07-03 23:10:19 +0530

    third  commit

--- game.py
+++ game.py
@@ -1,8 +1,6 @@
-# import random module
+
 import random
 
-# print multiline instruction
-# performstring concatenation of string
 print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
       + "Rock vs Paper -> Paper wins \n"
       + "Rock vs Scissors -> Rock wins \n"
@@ -11,21 +9,12 @@ print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
 while True:
 
     print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
-
-    # take the input from user
-
+ 
     choice = int(input("Enter your choice :"))
 
-    # OR is the short-circuit operator
-    # if any one of the condition is true
-    # then it return True value
-
-    # looping until user enter invalid input
     while choice > 3 or choice < 1:
         choice = int(input('Enter a valid choice please '))
 
-        # initialize value of choice_name variable
-    # corresponding to the choice value
     if choice == 1:
         choice_name = 'Rock'
     elif choice == 2:
@@ -33,13 +22,8 @@ while True:
     else:
         choice_name = 'Scissors'
 
-        # print user choice
     print('User choice is \n', choice_name)
     print('Now its Computers Turn....')
-
-    # Computer chooses randomly any number
-    # among 1 , 2 and 3. Using randint method
-    # of random module
     comp_choice = random.randint(1, 3)
 
    
@@ -90,6 +74,6 @@ while True:
     ans = input().lower()
     if ans == 'n':
         break
-# after coming out of the while loop
+#loop
 # we print thanks for playing
 print("thanks for playing")
